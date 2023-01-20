df947b55c787d8709592d1eb97102878ba67a5b8
bryan <bryan.kwan.developer@gmail.com>
2023-01-19 17:53:27 -0700

    Added basic paddle functionality; need to add scoring system and player 2 paddle control

--- software/pongers/obj/default/pongers.d
+++ software/pongers/obj/default/pongers.d
@@ -5,7 +5,8 @@ obj/default/pongers.o: pongers.c ../pongers_bsp//HAL/inc/io.h \
  ../pongers_bsp//HAL/inc/alt_types.h \
  ../pongers_bsp//HAL/inc/sys/alt_dev.h \
  ../pongers_bsp//HAL/inc/sys/alt_llist.h \
- ../pongers_bsp//HAL/inc/priv/alt_dev_llist.h
+ ../pongers_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../pongers_bsp//drivers/inc/altera_avalon_pio_regs.h
 
 ../pongers_bsp//HAL/inc/io.h:
 
@@ -24,3 +25,5 @@ obj/default/pongers.o: pongers.c ../pongers_bsp//HAL/inc/io.h \
 ../pongers_bsp//HAL/inc/sys/alt_llist.h:
 
 ../pongers_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../pongers_bsp//drivers/inc/altera_avalon_pio_regs.h:
--- software/pongers/pongers.c
+++ software/pongers/pongers.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <io.h>
 #include <system.h>
+#include <unistd.h>
 #include "altera_up_avalon_video_pixel_buffer_dma.h"
+#include "altera_avalon_pio_regs.h"
 
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 240
@@ -18,7 +20,35 @@ struct Rectangle {
 };
 typedef struct Rectangle Rectangle;
 
+// Updates paddle positions
+// There must be at least 2 paddles in the paddle[] array
+void update_paddle(Rectangle paddle[], int len) {
+	// Adjust speed according to user input
+	int SW = IORD(SW_BASE, 0);
+	SW = SW - 128;
+	printf("%SW value: d\n", SW);
+	if(SW>0) // SW is on
+		paddle[0].yspeed = -5;
+	else
+		paddle[0].yspeed = 5;
+	for(int i = 0; i< len; i++) {
+			// Update position
+			paddle[i].y += paddle[i].yspeed;
+			paddle[0].x = 0;
+			paddle[1].x = SCREEN_WIDTH - paddle[1].width;
+			// Check for collisions
+			if (paddle[i].y + paddle[i].height >= SCREEN_HEIGHT) {
+				paddle[i].y = SCREEN_HEIGHT - paddle[i].height;
+				paddle[i].yspeed = 0;
+			}
+			else if (paddle[i].y <= 0) {
+				paddle[i].y = 0;
+				paddle[i].yspeed = 0;
+			}
+	}
+}
 // Updates the position of each Rectangle object
+// "bounces" each object upon collision with screen edges
 void update_rect(Rectangle rect[], int len) {
 	for(int i = 0; i< len; i++) {
 		// Update position
@@ -69,15 +99,29 @@ int main()
 		printf ("Opened pixel buffer device \n");
 
 	// Game objects
-	int num_rectangles = 1;
-	Rectangle rect[num_rectangles];
-	Rectangle ball = {100, 100, 10, 5, 50, 50, 0xFFFF};
-	rect[0] = ball;
+	int num_balls = 1;
+	int BALL_WIDTH = 10;
+	int BALL_HEIGHT = 10;
+	int BALL_COLOUR = 0xFFFF;
+	Rectangle balls[num_balls];
+	Rectangle ball = {100, 100, 10, 5, BALL_WIDTH, BALL_HEIGHT, BALL_COLOUR};
+	balls[0] = ball;
+	int num_paddles = 2;
+	int PADDLE_WIDTH = 5;
+	int PADDLE_HEIGHT = 50;
+	int PADDLE_COLOUR = 0xFFFF;
+	Rectangle paddles[num_paddles];
+	Rectangle paddle_left = {0, 0, 0, 1, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_COLOUR};
+	Rectangle paddle_right = {SCREEN_WIDTH-PADDLE_WIDTH, 0, 0, 1, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_COLOUR};
+	paddles[0] = paddle_left;
+	paddles[1] = paddle_right;
 	while(1) {
 		// Game logic
-		update_rect(rect, num_rectangles);
+		update_rect(balls, num_balls);
+		update_paddle(paddles, num_paddles);
 		// Render the screen
-		draw(rect, num_rectangles, pixel_buf_dma_dev, 1);
+		draw(balls, num_balls, pixel_buf_dma_dev, 1);
+		draw(paddles, num_paddles, pixel_buf_dma_dev, 1);
 		usleep(10000);// 0.01sec
 	}
 	return 0;
