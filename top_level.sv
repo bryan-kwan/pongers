fe0655c49d7f9be3fbf68c3c58f60d226eff68cd
Ahmed73230 <83792507+Ahmed73230@users.noreply.github.com>
2023-02-22 17:13:43 -0700

    Add files via upload
    Need to test the frequency output to confirm some calculations

--- top_level.sv
+++ top_level.sv
@@ -1,2 +1,21 @@
+// top level module
+// Watch out for case sensitivity when translating from VHDL.
+// Also note that the .QSF is case sensitive.
+
 module top_level
+ (input  logic       clk,
+  input  logic       reset_n,
+  output logic       speaker);
+  
+  
+	//music box
+	logic tunes;              	
+	assign speaker = ~tunes;
+	
+	
+	logic outclk; 
+	pll clktest(.inclk0(clk), .c0(outclk));	//Need a 25MHz clk 
+	
+  //musicbox_v test(.clk(outclk), .speaker(tunes)); 
+	music_player musiK(.clk(outclk), .tunes(tunes));
 endmodule
