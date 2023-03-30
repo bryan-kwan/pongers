8d2aecb37774c08f15e9e9ec2fc8aa45325d841f
Benjamin Collier <collier.benjamin4@gmail.com>
2023-03-30 11:02:26 -0600

    audio

---
+++ sine_wave_audio/song.sv
@@ -0,0 +1,19 @@
+module song
+ (input logic clk,
+				  reset_n,
+  output logic [11:0] milisec,
+  output logic [9:0]	note);
+					
+always_comb begin
+
+note = 10'b0000000000;
+milisec = 500;
+
+
+		
+	
+		
+		
+end
+
+endmodule
